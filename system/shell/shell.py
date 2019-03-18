eaa475e6243cc7f7a38e5389a0367e56e05eccf9
Michal Stanislawski <mundzioch@wp.pl>
2019-03-18 19:47:43 +0100

    Complete restylish! Sorted classes, functions, init code.

--- system/shell/shell.py
+++ system/shell/shell.py
@@ -15,36 +15,6 @@ from client.client import Client
 
 
 
-# Constants
-SYS_PATH = os.path.normpath( os.path.abspath(os.getcwd()+"/..") )
-SYS_SHELL_PATH = os.path.normpath( SYS_PATH+"/shell" )
-SYS_APPS_PATH = os.path.normpath( SYS_PATH+"/apps" )
-
-ROOT_PATH = os.path.normpath( os.path.abspath(SYS_PATH+"/..") )
-ROOT_APPS_PATH = os.path.normpath( os.path.abspath(ROOT_PATH+"/apps") )
-
-# Something wrong with the working path -> show a sad message.
-if (not os.path.basename(SYS_PATH)=="system"
-    or not os.path.basename(os.getcwd())=="shell"):
-    errorMessage = """\
-==Error==
->Wrong or renamed directory!
-Please run `python shell.py` directly from it's directory.
-If it doesn't help, it means that the file is in wrong directory
- or dir has been renamed.
-Proper path to file should be:
-  `system/shell/shell.py`"""
-
-    print(errorMessage)
-    input("-Press [Enter] to dismiss-")
-    # Exit with error.
-    sys.exit(1)
-
-
-
-# Shell's command processor.
-Shell = CmdProcessor()
-
 # Shell manager.
 class ShellMan:
     """Manages shell actions like loading new shit, shutdown etc.""" # TODO:Clean.
@@ -122,19 +92,100 @@ class ShellMan:
         int n -- Code.
                  Raise a TypeError if n isn't int type.
 
-        Codes:
+        Negative values should be used only by system functions.
+        Codes, positive:
          1 - Shutdown system.
+        Codes, negative:
+         -1 - Wrong directory structure error.
         """
         if type(n)!=int: raise TypeError("code argument must be of int type.")
 
         if n==1: # 1 - shutdown
             ShellMan.shutdown()
 
+        elif n==-1:
+            ShellMan._fail_wrongPath()
 
-# == Load apps, commands =====================================================
+    def _fail_wrongPath():
+        errorMessage = """\
+==Error==
+>Wrong or renamed directory!
+Please run `python shell.py` directly from it's directory.
+If it doesn't help, it means that the file is in wrong directory
+ or dir has been renamed.
+Proper path to file should be:
+ `system/shell/shell.py`"""
 
+        print(errorMessage)
+        input("-Press [Return] to dismiss-")
+        # Exit with error.
+        sys.exit(1)
+
+# Server functions.
+class ServerMan:
+    def serverAcceptHandler(c, cData):
+        if not Running: return # Server shutdown - quit. See ShellMan.shutdown.
+
+        # Fulfill client's task in separate thread so main process can continue.
+        thread.start_new_thread(ServerMan.serverAccept, (c, cData))
+        # Continue to main loop.
+
+    def serverAccept(c, cData):   # TO-DO: CLEAN UP, FUCKER.
+        # cData - client data.
+        # c - client socket.
+        global Running
+        string = cData["string"] # Full command string.
+
+        if not len(string.split()): return None # Empty string.
+
+        if type(cData) == dict: # Success.
+            cmd = string.split()[0]
+
+            if Shell.isCmd(cmd): # Cmd exists.
+                # Run command.
+                ret = Shell.runString(cData["string"], cData)
+
+                if type(ret)==dict:
+                    if "msg" in ret:
+                        # "msg" - message for client.
+                        msg = str(ret["msg"])
+                        c.send(msg.encode())
+                    if "code" in ret:
+                        # "code" - certain shell action.
+                        code = ret["code"]
+                        ShellMan.code(code)
+            else:
+                c.send((cmd + ": command not found.").encode())
+
+            return True
+        else:                  # Client data syntax error.
+            print("An error occured: " + cData)
+            return False
+        c.close()
+
+# == INIT ====================================================================
+
+# Path constants.
+SYS_PATH = os.path.normpath( os.path.abspath(os.getcwd()+"/..") )
+SYS_SHELL_PATH = os.path.normpath( SYS_PATH+"/shell" )
+SYS_APPS_PATH = os.path.normpath( SYS_PATH+"/apps" )
+
+ROOT_PATH = os.path.normpath( os.path.abspath(SYS_PATH+"/..") )
+ROOT_APPS_PATH = os.path.normpath( os.path.abspath(ROOT_PATH+"/apps") )
+
+if (not os.path.basename(SYS_PATH)=="system"
+    or not os.path.basename(os.getcwd())=="shell" or True):
+    ShellMan.code(-1) # Wrong directory structure.
+
+
+# Load config file.
 ShellMan.loadConfig()
 
+
+# Init shell's command processor.
+Shell = CmdProcessor()
+
+# Load apps, commands -> Shell CmdProcessor.
 for i in os.listdir(SYS_APPS_PATH): # /system/apps.
     if os.path.isdir(SYS_APPS_PATH+"/"+i):
         ShellMan.loadApp(SYS_APPS_PATH+"/"+i)
@@ -144,75 +195,36 @@ for i in os.listdir(ROOT_APPS_PATH): # /apps.
         ShellMan.loadApp(ROOT_APPS_PATH+"/"+i)
 
 
-
-
-# == END =====================================================================
-
-
-
-
-# == SERVER CONFIG ===========================================================
+# Server config.
+SERVER_KEY = ShellMan.config["Server.Key"]
 SERVER_ADDRESS = ShellMan.config["Server.Address"]
 SERVER_PORT    = ShellMan.config["Server.Port"]
 SERVER_IP = (SERVER_ADDRESS, SERVER_PORT)
-SERVER_KEY = ShellMan.config["Server.Key"]
-
+# Log path's currently static.
 LOG_PATH = SYS_PATH+"/shell/logs/server.log"
 
-def serverAcceptHandler(c, cData):
-    if not Running: return # Server shutdown - quit. See ShellMan.shutdown.
-
-    # Fulfill client's task in separate thread so main process can continue.
-    thread.start_new_thread(serverAccept, (c, cData))
-    # Continue to main loop.
-
-def serverAccept(c, cData):   # TO-DO: CLEAN UP, FUCKER.
-    # cData - client data.
-    # c - client socket.
-    global Running
-    string = cData["string"] # Full command string.
-
-    if not len(string.split()): return None # Empty string.
-
-    if type(cData) == dict: # Success.
-        cmd = string.split()[0]
-
-        if Shell.isCmd(cmd): # Cmd exists.
-            # Run command.
-            ret = Shell.runString(cData["string"], cData)
-
-            if type(ret)==dict:
-                if "msg" in ret:
-                    # "msg" - message for client.
-                    msg = str(ret["msg"])
-                    c.send(msg.encode())
-                if "code" in ret:
-                    # "code" - certain shell action.
-                    code = ret["code"]
-                    ShellMan.code(code)
-        else:
-            c.send(cmd + ": command not found.".encode())
-
-        return True
-    else:                  # Client data syntax error.
-        print("An error occured: " + cData)
-        return False
-    c.close()
-
-
+# Init shell's server.
 Server = server.Server(SERVER_IP, SERVER_KEY, log=True, logfile=LOG_PATH)
 
-# == END =========================================================
 
-# cd home.
+# Go home. See ya!
 os.chdir(ROOT_PATH+"/home")
 
+# == INIT END ================================================================
 
 
-# Main program loop.
+
+
+
+# == PROGRAM LOOP ============================================================
 print("Current working dir:", os.getcwd())
 print("Running")
 
 Running = True
 while Running:
-    Server.accept(serverAcceptHandler, closeClient=False)
+    Server.accept(ServerMan.serverAcceptHandler, closeClient=False)
+
+# == END PROGRAM LOOP ========================================================
+
+
+# EOF
