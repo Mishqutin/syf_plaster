f91321eb83b13f45471a048d244585867f8acab0
Mishqutin <mundzioch@wp.pl>
2019-03-20 13:57:40 +0100

    lowercase commands

--- system/shell/libs/cmdproc.py
+++ system/shell/libs/cmdproc.py
@@ -2,24 +2,24 @@ from shlex import split as shlex_split
 
 
 class CmdProcessor:
-    
+
     def __init__(self, commands={}):
         self.commands = commands
-    
+
     def run(self, cmd, args, *argsv):
-        return self.commands[cmd](cmd, args, *argsv)
-    
+        return self.commands[cmd.lower()](cmd, args, *argsv)
+
     def runString(self, string, *argsv):
         if len(shlex_split(string))<1: return
-        
-        cmd = shlex_split(string)[0]
+
+        cmd = shlex_split(string)[0].lower()
         args = shlex_split(string)[1:]
-        
+
         return self.commands[cmd](cmd, args, *argsv)
-    
+
     def addCmd(self, name, func):
         self.commands[name]=func
-    
+
     def isCmd(self, name):
-        if name in self.commands: return True
+        if name.lower() in self.commands: return True
         else: return False
