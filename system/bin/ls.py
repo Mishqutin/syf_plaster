e3b01ad78a277a587cbb1c9a2c39dd1551147a4c
Mishqutin <mundzioch@wp.pl>
2019-04-21 22:34:32 +0200

    ls.py command tweaks, etc.

--- system/bin/ls.py
+++ system/bin/ls.py
@@ -11,11 +11,10 @@ class command:
         for i in os.listdir(p):
             file = p+"/"+i
             if os.path.isdir(file):
-                app = Colors.blue+i+Colors.reset
+                app = Colors.light_blue+i+Colors.reset
             else:
                 app = i
-            s+= app+" "
+            s+= app+"  "
         
-        print(s)
         return {"msg": s}
     
