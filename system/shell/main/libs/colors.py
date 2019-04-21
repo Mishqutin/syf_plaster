ba2022daa8d42446307354412938ac8e8e90576e
Mishqutin <mundzioch@wp.pl>
2019-04-21 22:22:43 +0200

    Updated colors.py lib with new colors

--- system/shell/main/libs/colors.py
+++ system/shell/main/libs/colors.py
@@ -8,4 +8,14 @@ class Colors:
     magenta = "\u001b[35m"
     cyan = "\u001b[36m"
     white = "\u001b[37m"
+    
+    light_black = gray = "\u001b[30;1m"
+    light_red = "\u001b[31;1m"
+    light_green = "\u001b[32;1m"
+    light_yellow = "\u001b[33;1m"
+    light_blue = "\u001b[34;1m"
+    light_magenta = "\u001b[35;1m"
+    light_cyan = "\u001b[36;1m"
+    light_white = "\u001b[37;1m"
+    
     reset = r = "\u001b[0m"
